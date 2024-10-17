351e7cab2e3f5cc20aa792fc9b9dad7c7eb6d281
yaren <yaren@DESKTOP-9U2BG79>
2024-10-18 01:21:56 +0300

    Add project files.

---
+++ Iki Sayinin Farkini Bulan CSharp Kodu/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iki_Sayinin_Farkini_Bulan_CSharp_Kodu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Konsolda sayıları direkt yanına yazmasını istediğim için WriteLine yerine Write yazdım.
+
+            Console.Write("Birinci Sayıyı Giriniz: ");
+            int Sayı1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("İkinci Sayıyı Giriniz: ");
+            int Sayı2 = Convert.ToInt32(Console.ReadLine());
+
+            int Fark = Sayı1 - Sayı2;
+
+            Console.Write("Girdiğiniz Sayıların Farkı: " + Fark);
+
+            Console.ReadKey();
+
+        }
+    }
+}
